badd910a2eb2ad6f9c33ecbe3cafcc9963ec5e68
Chrysolophus <jeromiescholten@HAL-9053.local>
2013-08-25 18:31:46 -0700

    Bee tracking protag

---
+++ bestiary/Bee.lua
@@ -0,0 +1,66 @@
+
+local sprite = require "sprite"
+local world = require "world"
+
+local BeeSheet = sprite.SpriteSheet("bestiary/BeeSheet.png", 2,2);
+local Bee = world.Sprite(0,0, 1, BeeSheet)
+
+function Bee:brain()
+
+	local animate = self:wrapLoop(function()
+		self:waitFrames(10) -- yields 10 times then returns
+	
+		self.frame = self.frame + 1
+		if self.frame > 2 then
+			self.frame = 1
+		end
+	end)
+	
+	local speed = 0.1875
+
+	while self:yield() do
+		animate()
+		
+        local vertBump = false
+
+        local hx, hy = self:locateHero()
+		
+        local diffx = self.x - hx
+        local diffy = self.y - hy
+        local magnitude = ( (diffx^2) +(diffy^2))^0.5 
+
+        local velx = diffx * speed / magnitude
+        local vely = diffy * speed / magnitude
+
+		self.vx = -velx 
+		
+		self:floatPhysics()
+		
+		if self.collideSide then
+			self.flip = not self.flip
+            self.vx = velx
+		end
+
+        if (diffx < 0 and not self.flip) then
+            self.flip = not self.flip
+        end
+        
+        if (diffx > 0 and self.flip) then
+            self.flip = not self.flip
+        end
+
+        if self.onGround or self.hitCeiling then
+			vertBump = true
+		end
+
+        self.vy = vertBump and vely or -vely + 0.1
+
+        print (velx, vely)
+
+        --print (hx, hy, diffx, diffy)
+
+	end
+end
+
+return Bee
+
